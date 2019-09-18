72ac3d6355d16ea8025fd70303a0625bc4c69d3b
Andrea Gaglione <a.gaglione7@gmail.com>
2019-09-18 12:35:40 +0200

    firstTry

--- sketch.js
+++ sketch.js
@@ -3,9 +3,29 @@ function preload(){
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+
+  frameRate(12);
+
+  background(100);
 }
 
 function draw() {
-  // put drawing code here
+  translate(width/2, height/2)
+  rotate(frameCount*3);
+  noFill();
+
+  stroke(
+    lerpColor(
+      color("aquamarine"),
+      color("tomato"),
+      frameCount/120));
+
+  ellipse(100, 0, 250);
+
+  if(frameCount==120){
+    noLoop();
+  }
+
 }
