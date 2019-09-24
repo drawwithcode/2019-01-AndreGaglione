ebf26545ce8d039860f2d4c234ceeeaeb274d9f7
Andrea Gaglione <a.gaglione7@gmail.com>
2019-09-25 00:20:23 +0200

    Esercizio

--- sketch.js
+++ sketch.js
@@ -3,25 +3,26 @@ function preload(){
 }
 function setup() {
 
-  createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES);
+  createCanvas(windowWidth,windowHeight);
+    angleMode(DEGREES);
 
-  frameRate(120);
-
-  background(200);
-
+    background(200);
+    frameRate(120);
 }
 
 function draw() {
-  translate(width/2, height/2);
-  rotate(frameCount*4);
-  noFill();
 
-  stroke(color="black");
-  line(-60, 0, cos(frameCount), sin(frameCount));
 
-  if(frameCount==90){
-    noLoop();
-  }
+    push();
+    textSize(20);
+        text("In viaggio verso Roma, non ci sono a lezione ma è per una buona causa", width/4, height/6.4);
+
+    var k = 50;
+
+    if(frameCount < 100){
+      translate(width/2,height/2);
+      stroke("black");
+      line(k, 0, (k)*cos(frameCount*4), (k)*sin(frameCount*4));
+    }
 
 }
